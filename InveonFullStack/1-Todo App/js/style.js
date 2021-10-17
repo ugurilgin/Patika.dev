fd07043ae5a061fc30aeb88ec9096d2bb494bc57
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2021-10-17 19:27:15 +0300

    checked and update check status in localstorage

--- InveonFullStack/1-Todo App/js/style.js
+++ InveonFullStack/1-Todo App/js/style.js
@@ -5,6 +5,7 @@ const createDate = document.querySelector("#createDate");
 const finishDate = document.querySelector("#finishDate");
 const gridContainer = document.querySelector(".grid-container");
 
+//Add Button Functions
 document.querySelector('.addButton').addEventListener('click',function(){
     
     try {
@@ -17,10 +18,12 @@ document.querySelector('.addButton').addEventListener('click',function(){
     a=getToday();
 });
  
-  
+//Close Button Functions
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='none';
 });
+
+//Cancel Button Functions
 document.querySelector('.cancelButton').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='none';
     
@@ -30,6 +33,9 @@ document.querySelector('.cancelButton').addEventListener('click',function(){
     finishDate.value="";
 });
 
+
+
+// Get Date Functions
 function getToday()
 {
     var date = new Date();
@@ -46,18 +52,18 @@ function getToday()
     return today;
 }
 
-let listStr = "";
+//Save Button Functions
 document.querySelector('.saveButton').addEventListener('click',function(){
+ 
+  let keyNumber=Math.floor(Math.random() * 1000000000000001);
     const element = document.createElement('div');
-
     element.className = 'grid-element';
-
     element.innerHTML='<div class="accordion">'+
     '<div class="alert" style="display: none;" >'+
     '<a  class="bell"> <i class="fa fa-bell-o" aria-hidden="true"></i>'+
     '</a> '+ 
 '</div>'+
-'<p>'+title.value+'</p>' +
+'<p>'+title.value+'<span hidden class="item-id" >'+keyNumber.toString()+'</span>'+'</p>' +
 '<p > <p class="left-column">Creation Time: '+createDate.value+'</p><p class="middle-column">Finish Time: '+finishDate.value+'</p><p class="right-column">Category: '+category.value+'</p> </p>'+
 
     '<div class="panelbuttons" >'+
@@ -70,10 +76,7 @@ document.querySelector('.saveButton').addEventListener('click',function(){
       '</div>'+
 
 '</div>';
-
-
 gridContainer.appendChild(element);
-
 document.querySelector('.bg-modal').style.display='none';
 const todo = {
   title: title.value,
@@ -82,32 +85,30 @@ const todo = {
   finishDate: finishDate.value,
   delete:false,
 }
-localStorage.setItem(localStorage.length, JSON.stringify(todo));
-
+localStorage.setItem(keyNumber, JSON.stringify(todo));
 title.value="";
 category.value="";
 createDate.value="";
 finishDate.value="";
-
-
-
-
 });
 
+//Get Items  Functions
 getItems();
 function getItems()
 {
   if (localStorage.length===0)
   {
     const element = document.createElement('div');
-
-  element.className = 'grid-element';
-
+    element.className = 'grid-element';
+   
+   
   element.innerHTML='<p class="remove-this">There is no record to show </p>';
   gridContainer.appendChild(element);
   }
-  for(let i=0;i<localStorage.length;i++)
-  {
+
+  Object.keys(localStorage).forEach(function(i){
+  
+   
   a=JSON.parse(localStorage.getItem(i));
   
   const element = document.createElement('div');
@@ -117,10 +118,8 @@ function getItems()
   date2=a.finishDate.split('/');
   date1 = new Date(date1[2],date1[1],date1[0]);
   date2 = new Date(date2[2],date2[1],date2[0]);
-  console.log(date2);
-  console.log(date1)
   let alertstr="";
-  if((date1 >= date2 )&&(a.delete===false))
+  if(date1 >= date2 )
   {
     alertstr='<div class="alert" style="display: block;" >';
   }
@@ -135,7 +134,7 @@ function getItems()
   '<a  class="bell"> <i class="fa fa-bell-o" aria-hidden="true"></i>'+
   '</a> '+ 
 '</div>'+
-'<p>'+a.title+'</p>' +
+'<p>'+a.title+'<span hidden class="item-id" >'+i.toString()+'</span>'+'</p>' +
 '<p > <p class="left-column">Creation Time: '+a.createDate+'</p><p class="middle-column">Finish Time: '+a.finishDate+'</p><p class="right-column">Category: '+a.category+'</p> </p>'+
 
   '<div class="panelbuttons" >'+
@@ -148,8 +147,55 @@ function getItems()
     '</div>'+
 
 '</div>';
-
-
+if (a.delete===true)
+   {
+  element.classList.toggle("checked");
+   }
 gridContainer.appendChild(element);
+
+});
 }
+//Check Button Functions
+let checks = document.getElementsByClassName('button-check');
+for(let i=0;i<checks.length;i++) {
+checks[i].addEventListener('click',function(){
+  let index=this.parentNode.parentNode.parentNode.getElementsByClassName("item-id")[0].innerHTML;
+  todoItem=JSON.parse(localStorage.getItem(index));
+  let ischecked=todoItem.delete;
+  if(ischecked===false)
+  {
+    ischecked=true;
+  }
+  else
+  {
+    ischecked=false;
+  }
+  const todo = {
+    title: todoItem.title,
+    category: todoItem.category,
+    createDate: todoItem.createDate,
+    finishDate: todoItem.finishDate,
+    delete:ischecked,
+  }
+  localStorage.setItem(index,JSON.stringify(todo));
+   this.parentNode.parentNode.parentNode.classList.toggle('checked');
+   
+});
+}
+//Edit Button Functions
+let edits = document.getElementsByClassName('button-edit');
+for(let i=0;i<checks.length;i++) {
+  edits[i].addEventListener('click',function(){
+  var x = this.parentNode.parentNode;
+  console.log(x); 
+});
+}
+
+//Delete Button Functions
+let deletes = document.getElementsByClassName('button-delete');
+for(let i=0;i<checks.length;i++) {
+  deletes[i].addEventListener('click',function(){
+  var x = this.parentNode.parentNode;
+  console.log(x); 
+});
 }
