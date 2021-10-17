0e78f2ded4bc4500644e3f338f497f1fb81203ed
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2021-10-17 11:57:17 +0300

    grid element changed

--- InveonFullStack/1-Todo App/js/style.js
+++ InveonFullStack/1-Todo App/js/style.js
@@ -1,5 +1,7 @@
 
+
 document.querySelector('.addButton').addEventListener('click',function(){
+ 
     document.querySelector('.bg-modal').style.display='flex';
     getToday();
 });
@@ -10,6 +12,14 @@ document.querySelector('.close').addEventListener('click',function(){
 });
 document.querySelector('.cancelButton').addEventListener('click',function(){
     document.querySelector('.bg-modal').style.display='none';
+    const title = document.querySelector("#title");
+    const category = document.querySelector("#category");
+    const createDate = document.querySelector("#createDate");
+    const finishDate = document.querySelector("#finishDate");
+    title.value="";
+    category.value="";
+    createDate.value="";
+    finishDate.value="";
 });
 
 function getToday()
@@ -26,4 +36,48 @@ function getToday()
     var today = day + "-" + month + "-" + year ;       
     document.getElementById("createDate").value = today;
 }
+const title = document.querySelector("#title");
+const category = document.querySelector("#category");
+const createDate = document.querySelector("#createDate");
+const finishDate = document.querySelector("#finishDate");
+const gridContainer = document.querySelector(".grid-container");
+let listStr = "";
+document.querySelector('.saveButton').addEventListener('click',function(){
+    const element = document.createElement('div');
+
+    element.className = 'grid-element';
+
+    element.innerHTML='<div class="accordion">'+
+    '<div class="alert" style="display: none;" >'+
+    '<a  class="bell"> <i class="fa fa-bell-o" aria-hidden="true"></i>'+
+    '</a> '+ 
+'</div>'+
+'<p>'+title.value+'</p>' +
+'<p > <p class="left-column">Creation Time: '+createDate.value+'</p><p class="middle-column">Finish Time: '+finishDate.value+'</p><p class="right-column">Category: '+category.value+'</p> </p>'+
+
+    '<div class="panelbuttons" >'+
+        '<a  class="button-check"> <i class="fa fa-check-square-o" aria-hidden="true"></i>'+
+        '</a>'+
+        '<a  class="button-edit"> <i class="fa fa-pencil-square-o" aria-hidden="true"></i>'+
+        '</a>'+
+        '<a  class="button-delete"> <i class="fa fa-trash-o" aria-hidden="true"></i>'+
+        '</a>'+
+      '</div>'+
+
+'</div>';
+
+
+gridContainer.appendChild(element);
+
+document.querySelector('.bg-modal').style.display='none';
+title.value="";
+category.value="";
+createDate.value="";
+finishDate.value="";
+
+
+
+
+});
+
 
